e89ee230ba614005c564d108e203821c635e592e
Andriy Khomych <andriy.khomych@gmail.com>
2019-03-28 13:06:36 +0200

    Reorganize project

--- webroot/db.php
+++ webroot/db.php
@@ -1,14 +1,14 @@
 <?php
 
 $servername = "mysql";
-$username = "testdrive";
-$password = "testdrive";
+$username = "web";
+$password = "web";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=testdrive", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=web", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    echo "Connected successfully";
 }
 catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
